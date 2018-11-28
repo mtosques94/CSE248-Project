9760a02a1b81180f7e7a9339829d6159c6bedbdf
mtosques94 <mtosques94@gmail.com>
2018-11-28 13:55:41 -0500

    login button now launches chess board activity

--- deepbleu-android/app/src/main/java/com/example/mtosq/deepbleu/LoginActivity.java
+++ deepbleu-android/app/src/main/java/com/example/mtosq/deepbleu/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.mtosq.deepbleu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void displayChessBoard (View view){
+        Intent intent = new Intent (this, ChessBoardActivity.class);
+        startActivity(intent);
     }
 }
