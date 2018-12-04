75349f8ba1941cdc7b13e648f7a958896cf4021d
mtosques94 <mtosques94@gmail.com>
2018-12-04 17:13:51 -0500

    added read / write methods to NetworkPlayer

--- deepbleu-android/app/src/main/java/com/example/mtosq/deepbleu/NetworkPlayer.java
+++ deepbleu-android/app/src/main/java/com/example/mtosq/deepbleu/NetworkPlayer.java
@@ -14,16 +14,13 @@ import com.google.gson.Gson;
 public class NetworkPlayer extends Player {
 
     private static Socket clientConnection = new Socket();
-
-    public static synchronized void setClientConnection(Socket clientConnection){
-        NetworkPlayer.clientConnection = clientConnection;
-    }
+    private static BufferedReader buffIn = null;
+    private static BufferedWriter buffOut = null;
 
     Gson gson = new Gson();
 
     public NetworkPlayer(String name, boolean isWhite) {
         super(name, isWhite);
-        //clientConnection = new Socket();
     }
 
     public NetworkPlayer(String name, boolean isWhite, Socket clientConnection) {
@@ -36,10 +33,11 @@ public class NetworkPlayer extends Player {
             InetAddress ia = InetAddress.getByName("10.0.2.2");
             SocketAddress sa = new InetSocketAddress(ia, 1994);
             clientConnection.connect(sa);
+            buffIn = new BufferedReader(new InputStreamReader(clientConnection.getInputStream()));
+            buffOut = new BufferedWriter(new OutputStreamWriter(clientConnection.getOutputStream()));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public void connect(String addr, int port) {
@@ -47,10 +45,11 @@ public class NetworkPlayer extends Player {
             InetAddress ia = InetAddress.getByName(addr);
             SocketAddress sa = new InetSocketAddress(ia, port);
             clientConnection.connect(sa);
+            buffIn = new BufferedReader(new InputStreamReader(clientConnection.getInputStream()));
+            buffOut = new BufferedWriter(new OutputStreamWriter(clientConnection.getOutputStream()));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public void connect(String addr, int port, String username, String password) {
@@ -58,17 +57,10 @@ public class NetworkPlayer extends Player {
             InetAddress ia = InetAddress.getByName(addr);
             SocketAddress sa = new InetSocketAddress(ia, port);
             clientConnection.connect(sa);
-            try {
-                BufferedWriter buffOut = new BufferedWriter(
-                        new OutputStreamWriter( this.getSocket().getOutputStream() ) );
-                AuthPair logMeIn = new AuthPair(username, password);
-                String moveJson = gson.toJson(logMeIn);
-                buffOut.write(moveJson);
-                buffOut.newLine();
-                buffOut.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            buffIn = new BufferedReader(new InputStreamReader(clientConnection.getInputStream()));
+            buffOut = new BufferedWriter(new OutputStreamWriter(clientConnection.getOutputStream()));
+            AuthPair logMeIn = new AuthPair(username, password);
+            this.writeJson(logMeIn);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -76,41 +68,45 @@ public class NetworkPlayer extends Player {
 
     }
 
-    public Socket getSocket() {
-        return clientConnection;
+    public void writeLine(String line) {
+        try {
+            buffOut.write(line.trim());
+            buffOut.newLine();
+            buffOut.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeJson(Object obj) {
+        String line = gson.toJson(obj);
+        this.writeLine(line);
+    }
+
+    public String readLine() {
+        String line = null;
+
+        while (line == null) {
+            try {
+                line = buffIn.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Network player read this line: " + line);
+        return line;
     }
 
     @Override
     public ChessMove getMove(Board b) {
-        try {
-            ChessMove mostRecentMove = new ChessMove(b.moveHistory.get(b.moveHistory.size()-1));
+        if(b.moveHistory.size() > 0) {
+            ChessMove mostRecentMove = new ChessMove(b.moveHistory.get(b.moveHistory.size() - 1));
             mostRecentMove.enemyCaptured = null;
-            gson.toJson(mostRecentMove);
-            try {
-                BufferedWriter buffOut = new BufferedWriter(
-                        new OutputStreamWriter( this.getSocket().getOutputStream() ) );
-                String moveJson = gson.toJson(mostRecentMove);
-                System.out.println("Writing ChessMove json to network...");
-                System.out.println(moveJson);
-                buffOut.write(moveJson);
-                buffOut.newLine();
-                buffOut.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            InputStreamReader isr = new InputStreamReader(clientConnection.getInputStream());
-            BufferedReader reader = new BufferedReader(isr);
-            System.out.println("NetworkPlayer trying to read line...");
-            String line = null;
-            while (line == null)
-                line = reader.readLine();
-            System.out.println("Network player read this line: " + line);
-            ChessMove networkMove = gson.fromJson(line, ChessMove.class);
-            return networkMove;
-        } catch (IOException e) {
-            return null;
+            this.writeJson(mostRecentMove);
         }
+        System.out.println("NetworkPlayer trying to read line...");
+        ChessMove networkMove = gson.fromJson(this.readLine(), ChessMove.class);
+        return networkMove;
     }
 
 }
